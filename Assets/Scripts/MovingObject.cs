afe610ebe52f5d9fe63ecabcc515b4f0d01d815a
Unknown <layers1995@gmail.com>
2018-02-01 09:38:21 -0600

    Add a few sprites to the game

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -18,6 +18,7 @@ public abstract class MovingObject : MonoBehaviour {
 
     //private BoxCollider2D boxCollider; // Declare a variable for the box collider of the moving object
     private Rigidbody2D rb2d; // Declare a variable for the rigidbody of the moving object
+    private BoxCollider2D boxCollider;
     private Transform groundCheck;
     private bool grounded = false;
     private bool rolling = false;
@@ -28,6 +29,7 @@ public abstract class MovingObject : MonoBehaviour {
     protected virtual void Start() {
         //boxCollider = GetComponent<BoxCollider2D>(); // Get the box collider componenet
         rb2d = GetComponent<Rigidbody2D>(); // Get the rigidbody component
+        boxCollider = GetComponent<BoxCollider2D>();
         groundCheck = transform.Find("groundCheck");
 
         // The bitshift gets the bitmask of the layer. This checks only the Ground layer
@@ -41,6 +43,7 @@ public abstract class MovingObject : MonoBehaviour {
         float moveVertical = 0f;
 
         grounded = isGrounded();
+        //Debug.Log(grounded);
 
         if (Input.GetButtonDown("Jump") && grounded && !rolling)
         {
@@ -66,29 +69,40 @@ public abstract class MovingObject : MonoBehaviour {
             transform.localScale = new Vector2(1f, 1f);
         }
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        if (false)
+        {
+            finalCollisionCheck();
+            rb2d.velocity = new Vector2(0, moveVertical);
+            return;
+        }
+        else
+        {
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rb2d.AddForce(movement * acceleration); // add the current movement force to the player
 
-        rb2d.AddForce(movement * acceleration); // add the current movement force to the player
-        if (Math.Abs(rb2d.velocity.x) > maxCrouchSpeed && crouching)
-        {
-            limitSpeed("x", rb2d, maxCrouchSpeed);
-        }
-        // limit speed if moving faster than max speed and not rolling
-        else if (Math.Abs(rb2d.velocity.x) > maxMoveSpeed && !rolling)
-        {
-            limitSpeed("x", rb2d, maxMoveSpeed);
-        }
-        // limit speed if rolling faster than max roll speed
-        else if (Math.Abs(rb2d.velocity.x) > maxRollSpeed)
-        {
-            limitSpeed("x", rb2d, maxRollSpeed);
+            if (Math.Abs(rb2d.velocity.x) > maxCrouchSpeed && crouching)
+            {
+                limitSpeed("x", rb2d, maxCrouchSpeed);
+            }
+            // limit speed if moving faster than max speed and not rolling
+            else if (Math.Abs(rb2d.velocity.x) > maxMoveSpeed && !rolling)
+            {
+                limitSpeed("x", rb2d, maxMoveSpeed);
+            }
+            // limit speed if rolling faster than max roll speed
+            else if (Math.Abs(rb2d.velocity.x) > maxRollSpeed)
+            {
+                limitSpeed("x", rb2d, maxRollSpeed);
+            }
+
+            // limit speed if moving faster than max vertical speed
+            if (Math.Abs(rb2d.velocity.y) > maxVerticalSpeed)
+            {
+                limitSpeed("y", rb2d, maxVerticalSpeed);
+            }
         }
 
-        // limit speed if moving faster than max vertical speed
-        if (Math.Abs(rb2d.velocity.y) > maxVerticalSpeed)
-        {
-            limitSpeed("y", rb2d, maxVerticalSpeed);
-        }
+  
     }
 
     // Function to limit the speed of the character if they move too fast in any direction.
@@ -115,7 +129,8 @@ public abstract class MovingObject : MonoBehaviour {
         else
         {
             throw new Exception("The limit speed function was called with a dimension that isn't x or y");
-        } 
+        }
+        return;
     }
 
     void roll()
@@ -143,11 +158,35 @@ public abstract class MovingObject : MonoBehaviour {
         }
     }
 
+    private void finalCollisionCheck()
+    {
+        // Get the velocity
+        Vector2 moveDirection = new Vector2(rb2d.velocity.x * Time.fixedDeltaTime + 1, 0.2f);
+
+        // Get bounds of Collider
+        var bottomRight = new Vector2(boxCollider.bounds.max.x, boxCollider.bounds.max.y);
+        var topLeft = new Vector2(boxCollider.bounds.min.x, boxCollider.bounds.min.y);
+
+        // Move collider in direction that we are moving
+        bottomRight += moveDirection;
+        topLeft += moveDirection;
+
+        // Check if the body's current velocity will result in a collision
+        if (Physics2D.OverlapArea(topLeft, bottomRight, groundLayer))
+        {
+            Debug.Log(rb2d.velocity.y);
+            // If so, stop the movement
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+        else
+            Debug.Log("Stop printing");
+    }
+
     bool isGrounded()
     {
         Vector2 startPosition = rb2d.transform.position;
-        Vector2 endPosition = new Vector2(rb2d.transform.position.x, rb2d.transform.position.y - 1);
-
+        Vector2 endPosition = new Vector2(rb2d.transform.position.x, rb2d.transform.position.y - 1.35f);
+        Debug.Log(boxCollider.size.y / 2f);
         RaycastHit2D hit = Physics2D.Linecast(startPosition, endPosition, groundLayer);
 
         if (hit.collider != null)
