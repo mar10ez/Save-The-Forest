7ab3896b7467c05ca392f4e3688c2f9b2aadd855
Unknown <layers1995@gmail.com>
2018-02-05 22:09:29 -0600

    Get the rest of basic animations working
    crouching, jumping, climbing

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -11,75 +11,75 @@ public abstract class MovingObject : MonoBehaviour {
     public float maxRollSpeed = 25f; // Max speed of the character while rolling
     public float maxVerticalSpeed = 10f; // how quickly the y coordinate of the character can change
     public float jumpSpeed = 5f; // How much force is applied when the character jumps
+    public float maxClimbSpeed = 1f;
     public int rollLength = 41; // how many frames a roll lasts
     public int attackLength = 10;
+    public int landingLag = 10; // After landing from a jump, how long does it take before the player can move again
+    
 
     public LayerMask groundLayer; // The ground layers contains all terrain that the player might touch
+    public LayerMask wallLayer;
 
 
     //private BoxCollider2D boxCollider; // Declare a variable for the box collider of the moving object
     private Rigidbody2D rb2d; // Declare a variable for the rigidbody of the moving object
     private BoxCollider2D boxCollider;
-    private Transform groundCheck;
     private Animator animator;
     private bool grounded = false;
     private bool rolling = false;
     private bool running = false;
+    private bool climbing = false;
     private int rollTimer = 0; // timer to tell when the roll is over
+    private bool crouching = false;
     private bool sneaking = false; // whether the player is sneaking
     private bool attacking = false; // whether the player is attacking
     private int attackTimer = 0; // timer to tell when the attack is over
-    private float moveHorizontal = 0f;
+
+    private float moveHorizontal;
+    private float moveVertical;
 
     // Use this for initialization
     protected virtual void Start() {
         //boxCollider = GetComponent<BoxCollider2D>(); // Get the box collider componenet
         rb2d = GetComponent<Rigidbody2D>(); // Get the rigidbody component
         boxCollider = GetComponent<BoxCollider2D>();
-        groundCheck = transform.Find("groundCheck");
         animator = GetComponent<Animator>();
 
         // The bitshift gets the bitmask of the layer. This checks only the Ground layer
         // The opposite of this is ~groundLayer, which checks all layers except the Ground layer.
         // More info at https://docs.unity3d.com/Manual/Layers.html
         groundLayer = 1 << LayerMask.NameToLayer("Ground");
+        
         //groundLayer = ~groundLayer;
+        wallLayer = 1 << LayerMask.NameToLayer("Wall");
+
+        moveHorizontal = 0f;
+        moveVertical = 0f;
     }
 
     void FixedUpdate() {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = 0f;
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = 0f;
 
-        if (moveHorizontal != 0)
-            running = true;
-        else
-            running = false;
-
-        // Make the player character face the direction it's moving
-        if (moveHorizontal > 0)
-            transform.localScale = new Vector2(1, transform.localScale.y);
-        else if (moveHorizontal < 0)
-            transform.localScale = new Vector2(-1, transform.localScale.y);
-
-        grounded = isGrounded();
-
-        if (moveHorizontal != 0 && grounded && !sneaking)
-            animator.SetBool("running", true);
-        else
-            animator.SetBool("running", false);
-
-        if (Input.GetButtonDown("Jump") && grounded && !rolling)
-        {
-            moveVertical = jumpSpeed;
-        }
-
+        grounded = isGrounded(); // Check if the player is grounded, this is one of the first things that should happen in a frame
+        run();
+        jump();
         roll();
         attack();
         sneak();
+        climbWall();
+
+        if (canClimbWall())
+            moveVertical = Input.GetAxis("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * acceleration); // add the current movement force to the player
 
+        checkSpeedLimits();
+    }
+
+    void checkSpeedLimits()
+    {
         if (Math.Abs(rb2d.velocity.x) > maxsneakSpeed && sneaking)
         {
             limitSpeed("x", rb2d, maxsneakSpeed);
@@ -95,37 +95,70 @@ public abstract class MovingObject : MonoBehaviour {
             limitSpeed("x", rb2d, maxRollSpeed);
         }
 
+        if (Math.Abs(rb2d.velocity.y) > maxClimbSpeed && canClimbWall())
+        {
+            limitSpeed("y", rb2d, maxClimbSpeed);
+        }
         // limit speed if moving faster than max vertical speed
-        if (Math.Abs(rb2d.velocity.y) > maxVerticalSpeed)
+        else if (Math.Abs(rb2d.velocity.y) > maxVerticalSpeed)
         {
             limitSpeed("y", rb2d, maxVerticalSpeed);
         }
-  
+    }
+
+    void run()
+    {
+        if (moveHorizontal != 0)
+            running = true;
+        else
+            running = false;
+
+        // Make the player character face the direction it's moving
+        if (moveHorizontal > 0)
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        else if (moveHorizontal < 0)
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+
+        if (moveHorizontal != 0 && grounded && !sneaking)
+            animator.SetBool("running", true);
+        else
+            animator.SetBool("running", false);
+    }
+
+    void jump()
+    {
+        if (animator.GetBool("jumping"))
+            animator.SetBool("jumping", false);
+
+        if (Input.GetButtonDown("Jump") && grounded && !rolling)
+        {
+            animator.SetBool("jumping", true);
+            moveVertical = jumpSpeed;
+        }
     }
 
     void sneak()
     {
         if (Input.GetButtonDown("Sneak") && grounded && !rolling)
         {
-            sneaking = true;
+            crouching = true;
             transform.position = new Vector2(transform.position.x, transform.position.y - .5f);
-        }
-
-        if (sneaking)
-        {
             animator.SetBool("crouching", true);
         }
-
-        if (sneaking && (Input.GetButtonUp("Sneak") || !grounded))
+     
+        if (crouching && moveHorizontal != 0)
+        {
+            animator.SetBool("sneaking", true);
+        }
+        
+        if (crouching && (Input.GetButtonUp("Sneak") || !grounded))
         {
             sneaking = false;
-            animator.SetBool("crouching", false);
-        }
-
-        if (!running && grounded && sneaking)
-            animator.SetBool("sneaking", true);
-        else if (running && grounded && sneaking)
             animator.SetBool("sneaking", false);
+            crouching = false;
+            animator.SetBool("crouching", false);
+            
+        }
     }
 
     void attack()
@@ -133,7 +166,7 @@ public abstract class MovingObject : MonoBehaviour {
         // If the roll button is pressed and the character is grounded and the character isn't already rolling, then start the rolling timer and start rolling
         if ((Input.GetButtonDown("Attack") && grounded && !rolling && !sneaking && !attacking))
         {
-            animator.SetBool("attacking1", true);
+            animator.SetBool("attacking", true);
             attackTimer = attackLength;
             Debug.Log(attackTimer);
             attacking = true;
@@ -153,7 +186,7 @@ public abstract class MovingObject : MonoBehaviour {
             if (attackTimer == 0)
             {
                 attacking = false;
-                animator.SetBool("attacking1", false);
+                animator.SetBool("attacking", false);
             }
 
         }
@@ -177,9 +210,9 @@ public abstract class MovingObject : MonoBehaviour {
         else if (dimension == "y")
         {
             if (rb2d.velocity.y < 0f)
-                rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+                rb2d.velocity = new Vector2(rb2d.velocity.x, -maxSpeed);
             else if (rb2d.velocity.y > 0f)
-                rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+                rb2d.velocity = new Vector2(rb2d.velocity.x, maxSpeed);
         }
         else
         {
@@ -220,17 +253,29 @@ public abstract class MovingObject : MonoBehaviour {
 
     void climbWall()
     {
-        if (!touchingWall())
+        if (!canClimbWall())
+        {
+            animator.SetBool("climbing", false);
             return;
-
-        float moveVertical = Input.GetAxis("Vertical");
+        }
+            
+        else
+        {
+            float moveVertical = Input.GetAxis("Vertical");
+            animator.SetBool("climbing", true);
+        }  
     }
 
-    bool touchingWall()
+    // Check if the player is touching a wall, will be used for wall climbing
+    bool canClimbWall()
     {
         Vector2 startPosition = rb2d.transform.position;
-        Vector2 endPosition = new Vector2(rb2d.transform.position.x + 1, rb2d.transform.position.y);
-        RaycastHit2D hit = Physics2D.Linecast(startPosition, endPosition, groundLayer);
+        //startPosition = new Vector2(startPosition.x, startPosition.y + 1.35f);
+        float rotation = transform.localScale.x;
+        // If the player is facing right, check for walls to the right of the player
+
+        Vector2 endPosition = new Vector2(rb2d.transform.position.x + (.625f * rotation), rb2d.transform.position.y - 1.35f);
+        RaycastHit2D hit = Physics2D.Linecast(startPosition, endPosition, wallLayer);
 
         if (hit.collider != null)
         {
@@ -239,16 +284,22 @@ public abstract class MovingObject : MonoBehaviour {
         return false;
     }
 
+    // Check if the player is grounded. Used for jumping mechanics and the like
     bool isGrounded()
     {
-        Vector2 startPosition = rb2d.transform.position;
-        Vector2 endPosition = new Vector2(rb2d.transform.position.x, rb2d.transform.position.y - 1.35f);
-        RaycastHit2D hit = Physics2D.Linecast(startPosition, endPosition, groundLayer);
+        Vector2 startPosition = rb2d.transform.position; // get the current position of the player
+        Vector2 endPosition = new Vector2(rb2d.transform.position.x, rb2d.transform.position.y - 1.35f); // get the position of the player's feet
+        RaycastHit2D hit = Physics2D.Linecast(startPosition, endPosition, groundLayer); 
+        // draw a line directly beneath the player's feet. If it hits anything, we know the player is grounded
 
+        // If the player is grounded, tell the animator, then return false
         if (hit.collider != null)
         {
+            animator.SetBool("grounded", true);
             return true;
         }
+        // If the player isn't grounded, tell the animator, then return false
+        animator.SetBool("grounded", false);
         return false;
     }
 }
