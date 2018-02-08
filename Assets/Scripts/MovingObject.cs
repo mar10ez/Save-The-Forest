7253e62a53a3ca4413f05d3a33cde7d8ca553b3c
Unknown <layers1995@gmail.com>
2018-02-08 11:25:08 -0600

    Commit so Monica can look at code

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -13,17 +13,15 @@ public abstract class MovingObject : MonoBehaviour {
     public float jumpSpeed = 5f; // How much force is applied when the character jumps
     public float maxClimbSpeed = 1f;
     public int rollLength = 41; // how many frames a roll lasts
-    public int attackLength = 10;
     public int landingLag = 10; // After landing from a jump, how long does it take before the player can move again
     
-
     public LayerMask groundLayer; // The ground layers contains all terrain that the player might touch
     public LayerMask wallLayer;
 
 
-    //private BoxCollider2D boxCollider; // Declare a variable for the box collider of the moving object
+    //private BoxCollider2D playerCollider; // Declare a variable for the box collider of the moving object
     private Rigidbody2D rb2d; // Declare a variable for the rigidbody of the moving object
-    private BoxCollider2D boxCollider;
+    private BoxCollider2D playerCollider;
     private Animator animator;
     private bool grounded = false;
     private bool rolling = false;
@@ -32,7 +30,7 @@ public abstract class MovingObject : MonoBehaviour {
     private int rollTimer = 0; // timer to tell when the roll is over
     private bool crouching = false;
     private bool sneaking = false; // whether the player is sneaking
-    private bool attacking = false; // whether the player is attacking
+    public bool attacking = false; // whether the player is attacking
     private int attackTimer = 0; // timer to tell when the attack is over
 
     private float moveHorizontal;
@@ -40,9 +38,9 @@ public abstract class MovingObject : MonoBehaviour {
 
     // Use this for initialization
     protected virtual void Start() {
-        //boxCollider = GetComponent<BoxCollider2D>(); // Get the box collider componenet
+        //playerCollider = GetComponent<BoxCollider2D>(); // Get the box collider componenet
         rb2d = GetComponent<Rigidbody2D>(); // Get the rigidbody component
-        boxCollider = GetComponent<BoxCollider2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
 
         // The bitshift gets the bitmask of the layer. This checks only the Ground layer
@@ -110,8 +108,11 @@ public abstract class MovingObject : MonoBehaviour {
     {
         if (moveHorizontal != 0)
             running = true;
-        else
+        if (moveHorizontal == 0 && grounded)
+            rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
             running = false;
+        if (moveHorizontal == 0)
+        
 
         // Make the player character face the direction it's moving
         if (moveHorizontal > 0)
@@ -127,19 +128,16 @@ public abstract class MovingObject : MonoBehaviour {
 
     void jump()
     {
-        if (animator.GetBool("jumping"))
-            animator.SetBool("jumping", false);
-
-        if (Input.GetButtonDown("Jump") && grounded && !rolling)
+        if (Input.GetButton("Jump") && grounded && !rolling)
         {
-            animator.SetBool("jumping", true);
+            animator.SetTrigger("jumping");
             moveVertical = jumpSpeed;
         }
     }
 
     void sneak()
     {
-        if (Input.GetButtonDown("Sneak") && grounded && !rolling)
+        if (Input.GetButton("Sneak") && grounded && !rolling)
         {
             crouching = true;
             transform.position = new Vector2(transform.position.x, transform.position.y - .5f);
@@ -151,7 +149,7 @@ public abstract class MovingObject : MonoBehaviour {
             animator.SetBool("sneaking", true);
         }
         
-        if (crouching && (Input.GetButtonUp("Sneak") || !grounded))
+        if (crouching && (!Input.GetButton("Sneak") || !grounded))
         {
             sneaking = false;
             animator.SetBool("sneaking", false);
@@ -164,31 +162,16 @@ public abstract class MovingObject : MonoBehaviour {
     void attack()
     {
         // If the roll button is pressed and the character is grounded and the character isn't already rolling, then start the rolling timer and start rolling
-        if ((Input.GetButtonDown("Attack") && grounded && !rolling && !sneaking && !attacking))
+        if ((Input.GetButton("Attack") && grounded && !rolling && !sneaking))
         {
             animator.SetBool("attacking", true);
-            attackTimer = attackLength;
-            Debug.Log(attackTimer);
             attacking = true;
         }
 
-        // If the character is attacking
-        if (attacking)
+        if ((!Input.GetButton("Attack") && attacking) || !grounded || rolling || sneaking)
         {
-            // decrease attack timer by one
-            attackTimer -= 1;
-
-            // If the player is in the air, we want to stop attacking
-            if (!grounded)
-                attackTimer = 0;
-
-            // if the roll timer is 0, then stop attacking
-            if (attackTimer == 0)
-            {
-                attacking = false;
-                animator.SetBool("attacking", false);
-            }
-
+            animator.SetBool("attacking", false);
+            attacking = false;
         }
 
     }
@@ -224,7 +207,7 @@ public abstract class MovingObject : MonoBehaviour {
     void roll()
     {
         // If the roll button is pressed and the character is grounded and the character isn't already rolling, then start the rolling timer and start rolling
-        if ((Input.GetButtonDown("Roll") && grounded && !rolling && !sneaking && !attacking))
+        if ((Input.GetButton("Roll") && grounded && !rolling && !sneaking && !attacking))
         {
             animator.SetBool("rolling", true);
             rollTimer = rollLength;
@@ -302,4 +285,5 @@ public abstract class MovingObject : MonoBehaviour {
         animator.SetBool("grounded", false);
         return false;
     }
+
 }
---
+++ Assets/Scripts/PlayerPunch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPunch : MonoBehaviour {
+
+    private BoxCollider2D punchCollider;
+    private PlayerMovement playerScript;
+
+	// Use this for initialization
+	void Start () {
+        playerScript = transform.parent.GetComponent<PlayerMovement>();
+        punchCollider = this.gameObject.GetComponent<BoxCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerScript.attacking)
+            punchCollider.enabled = true;
+        else if (playerScript.attacking == false)
+            punchCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Looks like there was a collision");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Thank god that's over");
+    }
+}
